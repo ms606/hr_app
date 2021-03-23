d0e6819aab5fa3734eeb033697da526a4a561478
Mufaddal Shakir <muffi606@gmail.com>
2021-03-23 21:26:38 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     hrPool: {
         user: "noor15",
         password: "noor",
-        connectString: "103.245.193.156/orcl",
+        connectString: "/orcl",
         poolMin: 10,
         poolMax: 10,
         poolIncrement: 0
